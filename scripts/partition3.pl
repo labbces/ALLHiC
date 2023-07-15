be167c7a3de95fa203b86367e784ec1082671889
Diego Riano <diriano@gmail.com>
2023-07-15 19:56:31 -0300

    some small fixes to make the script return the same results in different runs

--- scripts/partition3.pl
+++ scripts/partition3.pl
@@ -42,17 +42,17 @@ while(<IN>){
 	chomp;
 	my @data = split(/\s+/,$_);
 	my $chrn = $data[1];
+	next if ($chrn !~ /^Chr/);
 	foreach my $i(3..$#data){
 		my $ctg = (split/,/,$data[$i])[1];
 		$ctgdb{$ctg}->{$chrn}++;
 		}
 	}
 close IN;
-
 my %chrdb; ### pre-defined cluster based on chromosomes of close-releative species
-foreach my $ctg (keys %ctgdb){
+foreach my $ctg (sort(keys(%ctgdb))){
 	my $count = 0;
-	foreach my $chrn (sort {$ctgdb{$ctg}->{$b}<=>$ctgdb{$ctg}->{$a}} keys %{$ctgdb{$ctg}}){
+	foreach my $chrn (sort {$ctgdb{$ctg}->{$b}<=>$ctgdb{$ctg}->{$a}} sort(keys(%{$ctgdb{$ctg}}))){
 		$count++;
 		next if($count>1);
 #		print "$ctg	$chrn	$ctgdb{$ctg}->{$chrn}\n";
@@ -60,13 +60,21 @@ foreach my $ctg (keys %ctgdb){
 		}
 	}
 
+print "\tLOG:  There are ". scalar(keys(%ctgdb))." contigs assigned to chromossomes\n";
+print "\tLOG:  There are ". scalar(keys(%chrdb))." chromossomes chromossomes with assigned contigs\n";
+open TEST, ">test3.chr2ctg.tbl";
+foreach my $c(sort(keys(%chrdb))){
+ foreach my $ctg(sort(split(/,/,$chrdb{$c}))){
+  print TEST "$c\t$ctg\n";
+ }
+}
 print STDOUT "END: Creating data structures to keep Allele.gene.table -- ". localtime ."\n";
 
 system("rm -rf $wrkd");
 system("mkdir -p $wrkd");
 
 
-print STDOUT "START: Separating assembled contigs per reference Chromosome -- ". localtime ."\n";
+print STDOUT "START: Separating assembled contigs and BAM hits per reference Chromosome -- ". localtime ."\n";
 
 #Index input BAM file
 if (-f $bam.".bai"){ $bam=$bam; print STDOUT "Index file for BAM $bam already present\n";}
@@ -82,9 +90,10 @@ else{
 
 #Separate contigs and alignments for each reference chrosomome
 my %fh_samout;
-foreach my $chrn (keys %chrdb){
+foreach my $chrn (sort(keys %chrdb)){
 	next if ($chrn !~ /^Chr/);
 	my @ctgdb  = split(/,/,$chrdb{$chrn});
+	print "\tLOG:  There are ". scalar(@ctgdb)." contigs for chromosome $chrn\n";
 	if (!-d "$wrkd/$chrn"){
 		system("mkdir -p $wrkd/$chrn");
 		}
@@ -101,20 +110,18 @@ foreach my $chrn (keys %chrdb){
 		system("samtools faidx $wrkd/$chrn/seq.fasta");
 		system("awk '{print \$1,1,\$2}' $wrkd/$chrn/seq.fasta.fai > $wrkd/$chrn/seq.fasta.bed");
 		}
-	foreach my $ctg (@ctgdb){
- 		open(BAM, "samtools view --regions-file $wrkd/$chrn/seq.fasta.bed $bam|") or die"";
- 		while(<BAM>){
- 	        	chomp;
- 			my ($c1,$c2) = (split/\s+/,$_)[2,6];
- 			my %tmpdb = (); $tmpdb{'='}++; ### need retain intra-contig links
- 			map {$tmpdb{$_}++} @ctgdb;
- 			next if(!exists($tmpdb{$c1}) or !exists($tmpdb{$c2}));
- 			$fh_samout{$chrn}=$chrn."FH";
- 			open($fh_samout, ">>$wrkd/$chrn/sample.clean.sam") or die"";
- 			print $fh_samout "$_\n";
- 			}
- 		close BAM;
-	        }
+	open(BAM, "samtools view --regions-file $wrkd/$chrn/seq.fasta.bed $bam|") or die"";
+ 	while(<BAM>){
+         	chomp;
+ 		my ($c1,$c2) = (split/\s+/,$_)[2,6];
+ 		my %tmpdb = (); $tmpdb{'='}++; ### need retain intra-contig links
+ 		map {$tmpdb{$_}++} @ctgdb;
+ 		next if(!exists($tmpdb{$c1}) or !exists($tmpdb{$c2}));
+ 		$fh_samout{$chrn}=$chrn."FH";
+ 		open($fh_samout, ">>$wrkd/$chrn/sample.clean.sam") or die"";
+ 		print $fh_samout "$_\n";
+ 		}
+ 	close BAM;
 
 	close $fh_samout{$chrn};
 	system("samtools view -bt $wrkd/$chrn/seq.fasta.fai $wrkd/$chrn/sample.clean.sam > $wrkd/$chrn/sample.clean.bam");
@@ -122,7 +129,7 @@ foreach my $chrn (keys %chrdb){
 	}
 
 print STDOUT "END: Separating assembled contigs per reference Chromosome -- ". localtime ."\n";
-print STDOUT "START: Separating BAM hits per reference Chromosome -- ". localtime ."\n";
+#print STDOUT "START: Separating BAM hits per reference Chromosome -- ". localtime ."\n";
 ### Read prunning BAM file
 #my %saw_ctg;
 #my %saw_chrn;
